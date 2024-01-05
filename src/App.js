dbb6f0f250d9af30f1030c3fa3221f43065e1dea
said7388 <mdabusaidriyaz@gmail.com>
2024-01-05 15:33:12 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [file, setFile] = useState();
+
+  const handleChangeFile = (e) => {
+    if (e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+        <input
+          type="file"
+          accept=".jpg,.jpeg,.png,.gif,.webp,.pdf,.docs"
+          onChange={handleChangeFile}
+          className="input-field"
+        />
+        {
+          file ?
+            <div className="file-info">
+              <p>
+                {file.name}
+              </p>
+              <p>
+                <span>Type:</span> {file.type}
+              </p>
+            </div>
+            :
+            <div>
+              <p className='choose-title'>Choose or drag and drop a file to upload</p>
+              <p className='accept-title'>
+                Accepted file types: .jpg, .jpeg, .png, .gif, .webp, .pdf, .docs
+              </p>
+            </div>
+        }
+      </div>
     </div>
   );
 }
